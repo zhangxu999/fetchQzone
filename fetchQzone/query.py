a2687e736d627ec5d4dfd8969a2c9d2b10f14e62
zhangxu1573@qq.com <zhangxu1573@qq.com>
2015-02-28 14:33:00 +0800

    测试

--- fetchQzone/query.py
+++ fetchQzone/query.py
@@ -5,9 +5,9 @@ class queryTool(object):
     """docstring for queryTool"""
     def __init__(self):    
         
-        sql_tmpfeed="create table tmp_feed select info,time,userID_id,feedID,nick from fetchQzone_feed,fetchQzone_nick limit 5;delete from tmp_feed;"
+        sql_tmpfeed="create table tmp_feed select info,time,userID_id,feedID from fetchQzone_feed limit 5;delete from tmp_feed;"
         sql_tmpcomment="create table tmp_comment select parent_id,come_id,to_id,info,fetchQzone_comment.time,rootID from fetchQzone_comment limit 5;delete from tmp_comment;"
-        sql_tmpnick="create table sql_tmpnick "
+        sql_tmpnick="create table tmp_nick select guest_id,nick from fetchQzone_nick limit 5;delete from tmp_nick; "
         for x in (sql_tmpfeed,sql_tmpcomment):
             try:
                 cursor.execute(x)
@@ -28,29 +28,35 @@ class queryTool(object):
         Result={}
         cursor = connection.cursor()
         if friend==None:
-            sql_feed="insert into tmp_feed select info,time,userID_id,feedID,(select nick from fetchQzone_nick where  guest_id=userID_id group by guest_id) as nick from fetchQzone_feed where userID_id=%s  order by time  desc limit %s,%s ;"%(user,start,num)
+            sql_feed="insert into tmp_feed select info,time,userID_id,feedID from fetchQzone_feed where userID_id=%s  order by time  desc limit %s,%s ;"%(user,start,num)
             sql_comment=" insert into tmp_comment select parent_id,come_id,to_id,fetchQzone_comment.info,fetchQzone_comment.time,rootID from tmp_feed,fetchQzone_comment where parent_id=feedID  order by IDinFeed "
-            sql_nick="insert into tmp_nick select guest_id,nick "
-            sql_FeedtoRe="select * from tmp_feed;"
-            sql_CommenttoRe="select parent_id,come_id,to_id,info,time,rootID,(select nick from fetchQzone_nick where  guest_id=come_id group by guest_id) as come_nick,(select nick from fetchQzone_nick where  guest_id=to_id group by guest_id) as to_nick from tmp_comment;"
+            sql_nick="insert into tmp_nick select qq,nick from (select distinct  qq from (select  come_id as qq from   tmp_comment union all select to_id as qq from tmp_comment union select userID_id as qq from tmp_feed)uni)uni2,fetchQzone_nick where guest_id=qq group by qq;"
+            sql_FeedtoRe="select info,time,userID_id,feedID,nick from tmp_feed join tmp_nick on userID_id=qq;"
+            sql_CommenttoRe="select parent_id,come_id,to_id,info,time,rootID,tmp_nick1.nick as come_nick, tmp_nick2.nick as to_nick from tmp_comment  join tmp_nick tmp_nick1 on tmp_nick1.qq=come_id join tmp_nick tmp_nick2 on tmp_nick2.qq=to_id  ;"
             cursor.execute("delete from tmp_feed;")
             cursor.execute(sql_feed)
             cursor.execute("delete from tmp_comment;")
             cursor.execute(sql_comment)
-            
+            cursor.execute("delete from tmp_nick;")
+            cursor.execute(sql_nick)
+
             cursor.execute(sql_FeedtoRe)
             Result["feeds"]=cursor.fetchall()
             cursor.execute(sql_CommenttoRe)
             Result["comments"]=cursor.fetchall()
         else:
             sql_comment=" insert into tmp_comment select parent_id,come_id,to_id,info,time,rootID from fetchQzone_comment where (come_id=%s and to_id=%s) OR (come_id=%s and to_id=%s ) order by parent_id,IDinFeed "%(user,friend,friend,user)
-            sql_Feed="insert into tmp_feed select fetchQzone_feed.info,time,userID_id,feedID,(select nick from fetchQzone_nick where  guest_id=userID_id group by guest_id) as come_nick from (select  distinct parent_id   from tmp_comment limit %s,%s )fee,fetchQzone_feed  where parent_id=feedID order by feedID,time"%(start,num)
-            sql_CommenttoRe="select parent_id,come_id,to_id,tmp_comment.info,tmp_comment.time,rootID,(select nick from fetchQzone_nick where guest_id=come_id group by guest_id) as come_nick,(select nick from fetchQzone_nick where  guest_id=to_id  group by guest_id) as to_nick from tmp_comment,tmp_feed where parent_id=feedID order by feedID;"
-            sql_FeedtoRe="select * from tmp_feed"
+            sql_Feed="insert into tmp_feed select fetchQzone_feed.info,time,userID_id,feedID from (select  distinct parent_id   from tmp_comment limit %s,%s )fee,fetchQzone_feed  where parent_id=feedID order by feedID,time"%(start,num)
+            sql_nick="insert into tmp_nick select qq,nick from (select distinct  qq from (select  come_id as qq from   tmp_comment union all select to_id as qq from tmp_comment union select userID_id as qq from tmp_feed)uni)uni2,fetchQzone_nick where guest_id=qq group by qq;"
+            sql_CommenttoRe="select parent_id,come_id,to_id,tmp_comment.info,tmp_comment.time,rootID,(select nick from tmp_nick where qq=come_id ) as come_nick,(select nick from tmp_nick where  qq=to_id  ) as to_nick from tmp_comment,tmp_feed where parent_id=feedID order by feedID;"
+            sql_FeedtoRe="select info,time,userID_id,feedID,(select nick from tmp_nick where userID_id=qq) from tmp_feed"
             cursor.execute("delete from tmp_comment;")
             cursor.execute(sql_comment)
             cursor.execute("delete from tmp_feed")
             cursor.execute(sql_Feed)
+            cursor.execute("delete from tmp_nick;")
+            cursor.execute(sql_nick)
+
             cursor.execute(sql_CommenttoRe)
             
             Result["comments"]=cursor.fetchall()        
