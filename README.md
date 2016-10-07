65efda8fd4a650d6cf3642c367609502fdf4cb1e
zhangxu <738285867@qq.com>
2016-10-07 20:58:09 +0800

    整理多余代码，

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # fetchQzone
 抓取多人QQ空间说说和评论。以便分析一个人在他的好友的空间中留下的轨迹，获取有用信息。
 
-部署在百度云上，详情及demo请移步：[http://fqzone.duapp.com](http://fqzone.duapp.com/)
-，
-使用Python/django/MySQL/开发
+使用Python2.7/django/MySQL/开发.
+
+目前功能：
+1. 亲密度分析
+2. 二度人脉分析
+3. 两人交互分析
+4. 活动时间分析
+5. 共同好友分析
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -28,6 +28,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+
+
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
